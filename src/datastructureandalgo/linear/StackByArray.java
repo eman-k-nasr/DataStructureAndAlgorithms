a2839220c22f80d9f76385ed56af24f4a8b3a4e0
eman <emankamalnasr96@gmail.com>
2022-01-27 21:52:53 +0200

    stack implemented with array

---
+++ src/datastructureandalgo/linear/StackByArray.java
@@ -0,0 +1,94 @@
+package datastructureandalgo.linear;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author eman kamal
+ */
+public class StackByArray<T> {
+
+    ArrayList<T> items;
+    int top = -1;
+    int size = 0;
+
+    public StackByArray(int size) {
+        this.size = size;
+        this.items = new ArrayList<>(size);
+    }
+
+    void push(T item) {
+        if (top + 1 == size) {
+            System.out.println("Stack Overflow");
+        } else {
+            top++;
+            if(items.size() > top){
+              items.set(top, item);
+            }else{
+              items.add(item);
+            }
+        }
+    }
+
+    T getTop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return null;
+        } else {
+            return items.get(top);
+        }
+    }
+
+    T pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty");
+            return null;
+        } else {
+            T item = getTop();
+            top--;
+            size--;
+            return item;
+        }
+    }
+
+    boolean isEmpty() {
+        return top == -1;
+    }
+
+    @Override
+    public String toString() {
+        String res = "[";
+        for (int i = top; i >= 0; i--) {
+            res += String.valueOf(items.get(i)) + " ";
+        }
+        res += "]";
+        return res;
+    }
+    
+    
+    public static void main(String[] args) {
+     StackByArray<Integer> s1 = new StackByArray<>(3);
+     s1.push(10);
+     s1.push(20);
+     s1.push(30);
+     System.out.println(s1);
+     System.out.println("top "+ s1.getTop());
+     System.out.println("size "+ s1.size);
+     int popped1 = s1.pop();
+     System.out.println("popping1 "+ popped1);
+     System.out.println(s1);
+     System.out.println("top "+ s1.getTop());
+     System.out.println("size "+ s1.size);
+     int popped2 = s1.pop();
+     System.out.println("popping2 "+ popped2);
+     System.out.println(s1);
+     System.out.println("top "+ s1.getTop());
+     System.out.println("size "+ s1.size);
+     int popped3 = s1.pop();
+     System.out.println("popping3 "+ popped3);
+     System.out.println(s1);
+     System.out.println("top "+ s1.getTop());
+     System.out.println("size "+ s1.size);
+     
+    }
+}
